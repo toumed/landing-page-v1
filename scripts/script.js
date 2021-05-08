93a7e53071b226212e0d25e47655e074d1465873
Taqwa <taqwaoumed@gmail.com>
2021-05-08 15:40:13 -0700

    Added existing files

---
+++ scripts/script.js
@@ -0,0 +1,95 @@
+// JavaScript Document
+
+
+//var $btn = $('button');
+var $nav = $('nav');
+//var $navUl = $('nav > ul');
+
+// $btn.click(function(){
+// 	$navUl.toggleClass('open');	
+// });
+
+// The following JS modified from 
+// JS found at this per: http://codepen.io/laviperchik/pen/dlcBt
+$.fn.accessibleDropDown = function () {
+    
+	var el = $(this);
+
+    /* Make dropdown menus keyboard accessible */
+
+    el.find('a').focus(function() {
+        $(this).parents("li").addClass("hover");
+    }).blur(function() {
+        $(this).parents("li").removeClass("hover");
+    });
+
+}
+//Hamburger Nav
+$nav.accessibleDropDown();
+
+document.querySelector( "#nav-toggle" )
+  .addEventListener( "click", function() {
+    this.classList.toggle( "active" );
+  });
+
+  //Animation from
+  //https://speckyboy.com/css-javascript-text-animation-snippets/
+  var TxtRotate = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+  };
+  
+  TxtRotate.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+  
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+  
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  
+    var that = this;
+    var delta = 300 - Math.random() * 100;
+  
+    if (this.isDeleting) { delta /= 2; }
+  
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+    }
+  
+    setTimeout(function() {
+      that.tick();
+    }, delta);
+  };
+  
+  window.onload = function() {
+    var elements = document.getElementsByClassName('txt-rotate');
+    for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-rotate');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtRotate(elements[i], JSON.parse(toRotate), period);
+      }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+    document.body.appendChild(css);
+  };
+
+
+  
